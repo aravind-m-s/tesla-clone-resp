2b5ba2d5f3d33524d5c787e0e441b121ed3501d4
Aravind <aravindmangattu38@gmail.com>
2022-09-23 11:55:51 +0530

    final

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openMenu(){
+    document.getElementById('menu-list').style.display = "block"
+}
+
+function closeMenu(){
+    document.getElementById('menu-list').style.display = "none"
+}
